b591120f9c267df664ec16241480b4b28e845cc7
Duong Tien dat <83082798+tiendat2208@users.noreply.github.com>
2022-11-08 23:05:55 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Bạn có muốn vào CLB ULIS IT",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+    "Nếu bạn thoát ra và không trả lời thì có nghĩa là bạn đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  titleModar: "Cảm ơn Bạn vì đồng ý",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Ulis IT biết là bạn sẽ đồng ý  ^^ </br> Ulis IT biết là bạn sẽ đồng ý  :)) </br> "Ulis IT biết là bạn sẽ đồng ý . </br>  Điều quan trọng phải nói 3 lần HEHEHEhE "',
 };
